409e9197dd13c1a01ed45f0a33a75ed8171995fa
Krishna <support.qrhub@gmail.com>
2026-09-06 17:17:10 +0530

    build(release): bump versionCode to 7 and versionName to 1.5 for production Play Store bundle

--- QR_HUB/app/build.gradle.kts
+++ QR_HUB/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.qr.hub"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.4"
+        versionCode = 7
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
